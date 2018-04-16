f8d43406d0bbdee2421a2cb18ef148107b3086f0
osolnikrobert <35930510+osolnikrobert@users.noreply.github.com>
2018-04-16 14:49:46 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+import os
+import jinja2
+import webapp2
+from models import Sporocilo
+from google.appengine.api import users
+
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
+
+
+class BaseHandler(webapp2.RequestHandler):
+
+    def write(self, *a, **kw):
+        return self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        return self.write(self.render_str(template, **kw))
+
+    def render_template(self, view_filename, params=None):
+        if params is None:
+            params = {}
+        template = jinja_env.get_template(view_filename)
+        return self.response.out.write(template.render(params))
+
+
+class MainHandler(BaseHandler):
+    def get(self):
+        user = users.get_current_user()
+
+        params = {}
+
+        if user:
+            params["logiran"] = True
+            params["user"] = user
+            logout_url = users.create_logout_url("/")
+            params["logout_url"] = logout_url
+        else:
+            params["logiran"] = False
+
+            login_url = users.create_login_url('/')
+
+            params["login_url"] = login_url
+
+        return self.render_template("index.html", params=params)
+
+class BlogHandler(BaseHandler):
+    def post(self):
+        kar_smo_vnesli = self.request.get('vneseno_sporocilo')
+
+        params = {
+            "ime": "Robert",
+            "sporocila":
+                [
+                    "1. prvo sporocilo",
+                    kar_smo_vnesli
+                ]
+        }
+
+        return self.render_template("blog.html", params=params)
+
+    def get(self):
+
+        params = {
+            "ime": "Robert",
+            "sporocila":
+            [
+                "1. prvo sporocilo",
+                "2. drugo sporocilo"
+            ]
+        }
+
+        return self.render_template("blog.html", params=params)
+
+class KalkulatorHandler(BaseHandler):
+    def post(self):
+        rezultat = self.request.get('vneseno_1stevilo' + 'vneseno_2stevilo')
+
+        params = {
+            [
+                rezultat
+            ]
+        }
+
+        return self.render_template("kalkulator.html", params=params)
+
+    def get(self):
+        return self.render_template("kalkulator.html")
+
+class BazaHandler(BaseHandler):
+    def post(self):
+        params = {
+        }
+
+        return self.render_template("blog.html", params=params)
+
+    def get(self):
+
+        params = {
+            "podatki_iz_baze": Sporocilo.query().fetch()
+        }
+        return self.render_template("baza.html", params=params)
+
+class VnosHandler(BaseHandler):
+    def post(self):
+        tisto_kar_smo_vnseli_v_vnos = self.request.get('vnos')
+        tisto_kar_smo_vnseli_v_avtor = self.request.get('avtor')
+
+        nov_vnos = Sporocilo(
+            vnos=tisto_kar_smo_vnseli_v_vnos,
+            avtor=tisto_kar_smo_vnseli_v_avtor
+        )
+        nov_vnos.put()
+
+        self.redirect("/baza")
+
+    def get(self):
+
+        params = {
+        }
+        return self.render_template("vnos-v-bazo.html", params=params)
+
+app = webapp2.WSGIApplication([
+    webapp2.Route('/', MainHandler),
+    webapp2.Route('/blog', BlogHandler),
+    webapp2.Route('/kalkulator', KalkulatorHandler),
+    webapp2.Route('/baza', BazaHandler),
+    webapp2.Route('/vnos_v_bazo', VnosHandler),
+], debug=True)
